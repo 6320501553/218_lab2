d0dd36c8988e6d9916f42613897e4a6b4d4d4d25
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-11 18:14:13 +0700

    add

--- 6320501553_2_5.c
+++ 6320501553_2_5.c
@@ -1,12 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int day,mont,ne;
+    int day,mont,ne,m;
     scanf("%d %d",day,mont);
-    int m[12]={31,28,31,30,31,30,31,31,30,31,30,31};
     if(day<=7&&mont<=12)
     {
-        ne=m[mont-1]-day;
-        printf("%d",m[mont]);
+        switch(mont)
+        {
+            case 1: ne=31; break;
+            case 2: ne=28; break;
+            case 3: ne=31; break;
+            case 4: ne=30; break;
+            case 5: ne=31; break;
+            case 6: ne=30; break;
+            case 7: ne=31; break;
+            case 8: ne=31; break;
+            case 9: ne=30; break;
+            case 10: ne=31; break;
+            case 11: ne=30; break;
+            case 12: ne=31; break;
+        }
+        m=(ne+day)%7;
+        if(m==0)
+            printf("7");
+        else
+            printf("%d",m);
     }
+    return 0;
 }
