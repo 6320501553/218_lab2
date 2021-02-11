a34ad8638fee051522ba200089069b013fd2853f
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-02-11 17:18:12 +0700

    add

---
+++ 6320501553_2_3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],i,max,j;
+    char b[4]={'A','B','C'},c[4];
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    fflush(stdin);
+    scanf("%s",&c);
+    for(i=0;i<3;i++)
+    {
+        if(a[i]>a[i+1])
+        {
+            max=a[i];
+            a[i]=a[i+1];
+            a[i+1]=max;
+        }
+    }
+    for(i=3;i>=0;i--)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(b[i]==c[j])
+                printf("%d ",a[i]);
+        }
+    }
+    return 0;
+}
